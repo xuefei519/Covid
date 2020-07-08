b2c885842c946764fe5578686e9d849029295fc8
Xuefei <xuefei.shi@mail.mcgill.ca>
2020-07-08 15:14:09 -0400

    update

---
+++ dist/precache-manifest.6c810cd4b62017b19ad92cbfec3d4296.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe0c2bf29a73bd3b51ae",
+    "url": "/css/app.ec9952fc.css"
+  },
+  {
+    "revision": "fe0c2bf29a73bd3b51ae",
+    "url": "/js/app.ada2b556.js"
+  },
+  {
+    "revision": "83a953597e3387159dd8",
+    "url": "/css/chunk-vendors.8e75abdc.css"
+  },
+  {
+    "revision": "83a953597e3387159dd8",
+    "url": "/js/chunk-vendors.8af17f9a.js"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "3f75f34da140afb0647fbd9c0d7eb05b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aff9283731d48f4bfb5579d2f049cc8",
+    "url": "/img/brand/Quebec_logo_lily.svg"
+  },
+  {
+    "revision": "0a55aa9401906456ba52e58006e535fe",
+    "url": "/data/cumulativeCases.json"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "24d6e9b31d9a8374fa4406983cf19116",
+    "url": "/data/currentTotalCases.json"
+  },
+  {
+    "revision": "25deb4c5d23bde6e5f96ad20861372f0",
+    "url": "/img/theme/mchi.png"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "337eef470e751d96819e2203cbe32282",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "ff2c77bb6f09a27b02b0672ce557e040",
+    "url": "/img/theme/header-logo.png"
+  },
+  {
+    "revision": "75df427e14882b1bd0c48e403f3ba393",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  }
+];
